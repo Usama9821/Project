e5abe6f16a83069c478959460dc8ef95f46fd1f1
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-12-08 17:12:50 +0500

    update

--- news_app/lib/homepage.dart
+++ news_app/lib/homepage.dart
@@ -15,34 +15,78 @@ class _homepageState extends State<homepage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          TextField(
-            controller: msg,
-          ),
-          const Divider(
-            height: 20,
-            color: Colors.white,
-          ),
-          ElevatedButton(
-            child: Text('Show in Center'),
-            onPressed: () {
-              setState(() {
-                showmessage = msg.text;
-              });
-            },
-          ),
-          const Divider(
-            height: 20,
-            color: Colors.white,
-          ),
-          Center(
-            child: Text(
-              showmessage,
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              child: Text('Menu'),
             ),
-          ),
-        ],
+            ListTile(
+              title: const Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Version Info'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('News'),
+            Text(
+              'App',
+              style: TextStyle(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
+        elevation: 0.2,
+        actions: [],
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: msg,
+            ),
+            const Divider(
+              height: 20,
+              color: Colors.white,
+            ),
+            ElevatedButton(
+              child: Text('Show in Center'),
+              onPressed: () {
+                setState(() {
+                  showmessage = msg.text;
+                });
+              },
+            ),
+            const Divider(
+              height: 20,
+              color: Colors.white,
+            ),
+            Center(
+              child: Text(
+                showmessage,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- news_app/lib/main.dart
+++ news_app/lib/main.dart
@@ -9,50 +9,7 @@ main() {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Simple AppBar'),
-            actions: const <Widget>[
-              Icon(Icons.search),
-            ],
-          ),
-          drawer: Drawer(
-            child: Column(
-              children: [
-                Text('Username'),
-                Text('Username'),
-              ],
-            ),
-          ),
-          body: homepage(),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: Icon(Icons.add),
-            backgroundColor: Colors.redAccent,
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home,
-                  color: Colors.yellow,
-                ),
-                label: 'Home',
-                backgroundColor: Colors.red,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.person,
-                  color: Colors.green,
-                ),
-                label: 'Account',
-                //title is depreciated instead we use label widget
-                //title: Text("Account"),
-
-                backgroundColor: Colors.yellow,
-              ),
-            ],
-          )),
+      home: homepage(),
     ),
   );
 }
