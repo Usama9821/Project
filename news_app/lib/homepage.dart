61a02c8362b2d5c28906de758fe030074f0b6380
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-12-08 16:23:00 +0500

    update

---
+++ news_app/lib/homepage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class homepage extends StatefulWidget {
+  const homepage({Key? key}) : super(key: key);
+
+  @override
+  _homepageState createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  var msg = TextEditingController();
+  String showmessage = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          TextField(
+            controller: msg,
+          ),
+          const Divider(
+            height: 20,
+            color: Colors.white,
+          ),
+          ElevatedButton(
+            child: Text('Show in Center'),
+            onPressed: () {
+              setState(() {
+                showmessage = msg.text;
+              });
+            },
+          ),
+          const Divider(
+            height: 20,
+            color: Colors.white,
+          ),
+          Center(
+            child: Text(
+              showmessage,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
