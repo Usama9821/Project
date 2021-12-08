c532553c4fb161789c22bb6cdfdbcc1e258161e6
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-12-08 19:47:53 +0500

    splash + launcher icon

--- news_app/lib/homepage.dart
+++ news_app/lib/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:animated_search_bar/animated_search_bar.dart';
 
 class homepage extends StatefulWidget {
   const homepage({Key? key}) : super(key: key);
@@ -10,8 +11,9 @@ class homepage extends StatefulWidget {
 }
 
 class _homepageState extends State<homepage> {
-  var msg = TextEditingController();
-  String showmessage = "";
+  // var msg = TextEditingController();
+  // String showmessage = "";
+  String searchText = "";
 
   @override
   Widget build(BuildContext context) {
@@ -42,56 +44,96 @@ class _homepageState extends State<homepage> {
         ),
       ),
       appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('News'),
-            Text(
-              'App',
-              style: TextStyle(
-                color: Colors.blue,
-              ),
-            ),
-          ],
-        ),
-        centerTitle: true,
-        elevation: 0.2,
-        actions: const [
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Icon(Icons.search),
+        title: AnimatedSearchBar(
+          label: "Click here to search",
+          labelStyle: TextStyle(fontSize: 14),
+          searchStyle: TextStyle(color: Colors.white60),
+          cursorColor: Colors.white,
+          searchDecoration: InputDecoration(
+            hintText: "Search",
+            alignLabelWithHint: true,
+            fillColor: Colors.white,
+            focusColor: Colors.white,
+            hintStyle: TextStyle(color: Colors.white70),
+            border: InputBorder.none,
           ),
-        ],
+          onChanged: (value) {
+            print("value on Change");
+            setState(() {
+              searchText = value;
+            });
+          },
+        ),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: msg,
-            ),
-            const Divider(
-              height: 20,
-              color: Colors.white,
-            ),
-            ElevatedButton(
-              child: Text('Show in Center'),
-              onPressed: () {
-                setState(() {
-                  showmessage = msg.text;
-                });
-              },
-            ),
-            const Divider(
-              height: 20,
-              color: Colors.white,
-            ),
-            Center(
-              child: Text(
-                showmessage,
+      // appBar: AppBar(
+      //   // title: Row(
+      //   //   mainAxisAlignment: MainAxisAlignment.center,
+      //   //   children: [
+      //   //     Text('News'),
+      //   //     Text(
+      //   //       'App',
+      //   //       style: TextStyle(
+      //   //         color: Colors.blue,
+      //   //       ),
+      //   //     ),
+      //   //   ],
+      //   // ),
+      //   centerTitle: true,
+      //   elevation: 0.2,
+      //   actions: const [
+      //     Padding(
+      //       padding: EdgeInsets.all(10.0),
+      //       child: Icon(Icons.search),
+      //     ),
+      //   ],
+      // ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.fromLTRB(50, 30, 70, 0),
+            child: Text(
+              'Welcome to',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 36,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ],
-        ),
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.fromLTRB(50, 0, 30, 100),
+            child: Row(
+              children: const [
+                Text(
+                  ' eNews ',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    backgroundColor: Colors.red,
+                    fontSize: 42,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Filter',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 42,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Divider(
+          //   height: 20,
+          //   color: Colors.white,
+          // ),
+        ],
       ),
     );
   }
