47251afdb389541d669a8b4a21b6b91e3a882483
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-12-08 22:33:07 +0500

    update

--- FlutterNewsApp-master/lib/views/article_view.dart
+++ FlutterNewsApp-master/lib/views/article_view.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ArticleView extends StatefulWidget {
-
   final String postUrl;
   ArticleView({@required this.postUrl});
 
@@ -13,8 +11,8 @@ class ArticleView extends StatefulWidget {
 }
 
 class _ArticleViewState extends State<ArticleView> {
-
-  final Completer<WebViewController> _controller = Completer<WebViewController>();
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,20 +21,22 @@ class _ArticleViewState extends State<ArticleView> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              "Flutter",
+              "News",
               style:
-              TextStyle(color: Colors.black87, fontWeight: FontWeight.w600),
+                  TextStyle(color: Colors.black87, fontWeight: FontWeight.w600),
             ),
             Text(
-              "News",
+              "App",
               style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
             )
           ],
         ),
         actions: <Widget>[
           Container(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-              child: Icon(Icons.share,))
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Icon(
+                Icons.share,
+              ))
         ],
         backgroundColor: Colors.transparent,
         elevation: 0.0,
@@ -45,8 +45,8 @@ class _ArticleViewState extends State<ArticleView> {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         child: WebView(
-          initialUrl:  widget.postUrl,
-          onWebViewCreated: (WebViewController webViewController){
+          initialUrl: widget.postUrl,
+          onWebViewCreated: (WebViewController webViewController) {
             _controller.complete(webViewController);
           },
         ),
