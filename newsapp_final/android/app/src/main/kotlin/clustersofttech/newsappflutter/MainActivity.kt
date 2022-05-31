c4d18ec65d4647e4aa2058917648966e4784d160
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2022-05-31 09:10:13 +0500

    update

--- newsapp_final/android/app/src/main/kotlin/clustersofttech/newsappflutter/MainActivity.kt
+++ newsapp_final/android/app/src/main/kotlin/clustersofttech/newsappflutter/MainActivity.kt
@@ -1,6 +1,5 @@
 package clustersofttech.newsappflutter
 
 import io.flutter.embedding.android.FlutterActivity
-
 class MainActivity: FlutterActivity() {
 }
--- newsapp_final/lib/main.dart
+++ newsapp_final/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:dcdg/dcdg.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
